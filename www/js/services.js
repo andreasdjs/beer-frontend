0da021a1117b649d2b4e0d9557b0501321dfdeac
andreasdjs <andreas.dagnell@gmail.com>
2016-06-16 11:20:41 +0200

    Cleaning.

--- www/js/services.js
+++ www/js/services.js
@@ -10,12 +10,6 @@ angular.module('app.services', [])
     $http.get('http://localhost\:3000/api/ratedMatchedBeerLocalStore').then(cb);
   };
 
-/* duplicate
-  this.getMatchedSystembolagetBeer = function(cb) {
-    $http.get('http://localhost\:3000/api/systembolagetMatchedBeer').then(cb);
-  };
-*/
-
   this.getMatchedSystembolagetBeer = function(cb) {
     $http.get('http://localhost\:3000/api/systembolagetMatchedBeer').then(cb);
   };
@@ -30,16 +24,8 @@ angular.module('app.services', [])
 .service('sharedData', function SharedData(){
 
 	var sharedData = this;
-//	sharedData.message = "Default";
 
 });
 
 
-/*
-.service('dataService', [function($http){
-  this.getRatedBeer = function(cb) {
-    $http.get('http://localhost\:3000/api/ratedBeer').then(cb);
-  };
-}]);
-*/
 
